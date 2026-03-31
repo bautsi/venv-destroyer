75c244f3f9a350c5bfd821854c4a71fa0ae95a2a
bautsi <b3r1a3dtw@gmail.com>
2026-03-31 12:14:13 +0800

    feat: add option flags handle logic at main

--- main.go
+++ main.go
@@ -3,15 +3,49 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func main() {
-	debugFlag := flag.Bool("debug", false, "Debug mode, .")
+// Global Variables
+var logSplitter = "======================================================"
 
+func main() {
+	// Mode Switch
+	debugFlag := flag.Bool("debug", false, "Debug mode, use with other flags.")
+
+	// Options
+	allOptionsFlag := flag.Bool("all", false, "Activate all types of files, use with other flags.")
+
+	withEnvFlag := flag.Bool("env", false, "Also handle env files, use with other flags.")
+	withCacheFlag := flag.Bool("cache", false, "Also handle cache files, use with other flags.")
+
+	// Actions
 	destroyFlag := flag.Bool("destroy", false, "Destroy all virtual environments.")
 	checkFlag := flag.Bool("check", false, "Check if any virtual environment exists.")
 
 	flag.Parse()
+
+	// Option Flags Handle
+	var addtKeys []string
+	if *allOptionsFlag {
+		*withEnvFlag = true
+		*withCacheFlag = true
+	}
+
+	if *withEnvFlag {
+		addtKeys = append(addtKeys, "env")
+	}
+	if *withCacheFlag {
+		addtKeys = append(addtKeys, "cache")
+	}
+
+	// Load Options
+	names, err := loadVenvNames(addtKeys)
+	if err != nil {
+		os.Exit(2)
+	}
+	fmt.Println(strings.Join(names, ", "))
 
 	// Debug Mode
 	if *debugFlag {
@@ -20,7 +54,7 @@ func main() {
 			return
 		}
 		if *checkFlag {
-			err := checkVenvDebug()
+			err := checkVenvDebug(names)
 			if err != nil {
 				fmt.Println("Check Virtual Environment Debug Error: ", err)
 			}
